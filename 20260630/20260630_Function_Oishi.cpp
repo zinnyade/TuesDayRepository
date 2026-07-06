d4b0b05a544e4496a915cd4db77128b1ab0c05c6
zinnyade <yoshie.ooisi@gmail.com>
2026-07-06 17:34:54 +0900

    課題完成

--- 20260630/20260630_Function_Oishi.cpp
+++ 20260630/20260630_Function_Oishi.cpp
@@ -4,33 +4,59 @@
 #include<ctime>
 using namespace std;
 
+//ゲーム本体
 void Game()
 {
+	//変数
 	int i;
 	int playerInput;
 	int cardNum = 0;
 	int playerCard = 0, cpuCard = 0;
 	
+	//乱数の初期化
 	srand((unsigned int)time(NULL));
 	
-	playerCard += CardPull();
+	//プレイヤーの初期カードの生成
+	CardPull(playerCard);
+	CardPull(playerCard);
+
+	//CPUの初期カードの生成
+	CardPull(cpuCard);
+	CardPull(cpuCard);
 	
+	//ゲームの説明
+	cout << "CPUとブラックジャックみたいなものをします。\n"
+		<< "カードの合計が　" << CARD_SUM_MAX << "　を超えると負けです。\n"
+		<< "カードの合計が　" << CARD_SUM_MAX << "　に近い方が勝ちです。\n"
+		<< "CPUはあなたよりカードの合計が少ない場合、カードを引きます。\n"
+		<< "あなたはカードを引くかどうか選択できます。\n";
 
-
+	//ゲームループ
+	while (playerCard < CARD_SUM_MAX && cpuCard < CARD_SUM_MAX)
+	{
+		//プレイヤーのカードの合計を表示
+		cout << "現在あなたのカードの合計は　" << playerCard << "　です。\n";
+		//プレイヤーのカードを引くかどうかの入力
+		playerInput = InputCheck();
+		//プレイヤーがカードを引く場合、カードを引く
+		if (playerInput == 0) CardPull(playerCard);
+		//CPUがプレイヤーよりカードの合計が小さい場合、カードを引く
+		if (cpuCard < playerCard) CardPull(cpuCard);
+		//判定
+		Judgement(playerCard, cpuCard);
+	}
+	//ゲーム終了時のカードの合計を表示
+	cout << "あなたのカードの合計は　" << playerCard << "　でした。\n"
+		<< "CPUのカードの合計は　" << cpuCard << "　でした。\n";
 }
 
-int CardPull()
+//カードを引く関数
+void CardPull(int& card)
 {
-	int cardNum = rand() % CARD_MAX + CARD_MIN;
-	return cardNum;
+	card += rand() % CARD_MAX + CARD_MIN;
 }
 
-int CardDisplay(int cardNum)
-{
-	
-}
-
-
+//入力チェック関数
 int InputCheck()
 {
 	int inputNum;
@@ -44,7 +70,16 @@ int InputCheck()
 	return inputNum;
 }
 
+//判定関数
+void Judgement(int playerCard, int cpuCard)
+{
 
+	if (playerCard == CARD_SUM_MAX && cpuCard == CARD_SUM_MAX || playerCard > CARD_SUM_MAX && cpuCard > CARD_SUM_MAX) cout << "引き分けです。\n";
+	else if (playerCard > CARD_SUM_MAX) cout << "あなたの負けです。\n";
+	else if (cpuCard > CARD_SUM_MAX) cout << "あなたの勝ちです。\n";
+	else if (playerCard == CARD_SUM_MAX) cout << "あなたの勝ちです。\n";
+	else if (cpuCard == CARD_SUM_MAX) cout << "あなたの負けです。\n";
+}
 
 
 
--- 20260630/20260630_Header_Oishi.h
+++ 20260630/20260630_Header_Oishi.h
@@ -6,15 +6,12 @@ const int INPUT_MIN = 0;
 const int CARD_MAX = 11;
 const int CARD_MIN = 1;
 const int START_CARD_MAX = 2;
-const int 
 
 //カード生成
-int CardPull();
+void CardPull(int& card);
 //入力チェック
 int InputCheck();
 //判定
-int Judgement();
+void Judgement(int playerCard, int cpuCard);
 //ゲーム
 void Game();
-//カード表示
-int CardDisplay(int cardNum);
